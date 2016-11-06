8ece5e3b88a36122c71a5272f63119a2c88edf27
meerajahmed <meerajahmed@gmail.com>
2016-11-06 17:16:34 +0530

    added layout mirroring support and content descriptions

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -102,6 +102,7 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.attachToRecyclerView(recyclerView);
+    fab.setContentDescription(getString(R.string.add_symbol));
     fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         if (isConnected){
